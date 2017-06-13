512342fa0feb9927bd43ed011e64ea63c7b26b97
Spencer Bruce <spencer.bruce@hpe.com>
2017-06-13 17:26:34 +0300

    Corrected version to 12.55.3

--- Hpe.Nga.Api.Core.Tests/Properties/AssemblyInfo.cs
+++ Hpe.Nga.Api.Core.Tests/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Hpe.Nga.Api.Core.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © HPE 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -44,5 +44,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("12.55.3.0")]
+[assembly: AssemblyFileVersion("12.55.3.0")]
