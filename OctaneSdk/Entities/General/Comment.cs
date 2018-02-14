497ac8a5ef6399e1eb909c1bfd24ab45506842d8
Paul Ciobanu <paul.ciobanu@hpe.com>
2018-02-14 15:13:54 +0200

    Move Comment under Entities/General folder

--- OctaneSdk/Entities/General/Comment.cs
+++ OctaneSdk/Entities/General/Comment.cs
@@ -1,9 +1,25 @@
-﻿namespace MicroFocus.Adm.Octane.Api.Core.Entities.WorkItems
+﻿/*!
+* (c) 2016-2018 EntIT Software LLC, a Micro Focus company
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+namespace MicroFocus.Adm.Octane.Api.Core.Entities
 {
     /// <summary>
     /// Wrapper for Comment entity.
     /// </summary>
-    public class Comment : WorkItem
+    public class Comment : BaseEntity
     {
         public const string AUTHOR_FIELD = "author";
         public const string OWNER_WORK_FIELD = "owner_work_item";
@@ -25,5 +41,7 @@
         public BaseEntity OwnerTest => (BaseEntity)GetValue(OWNER_TEST_FIELD);
 
         public BaseEntity OwnerRun => (BaseEntity)GetValue(OWNER_RUN_FIELD);
+
+        public string Text => GetStringValue(TEXT_FIELD);
     }
 }
