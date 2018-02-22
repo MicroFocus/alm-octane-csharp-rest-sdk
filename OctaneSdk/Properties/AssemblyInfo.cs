bada0e4dfb823c1c5a6ffc5a6bad36267d060835
Radislav Berkovich <radislav.berkovich@hpe.com>
2018-02-22 12:44:49 +0200

    increment version to 12.55.22

--- OctaneSdk/Properties/AssemblyInfo.cs
+++ OctaneSdk/Properties/AssemblyInfo.cs
@@ -48,8 +48,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.55.15.0")]
-[assembly: AssemblyFileVersion("12.55.15.0")]
+[assembly: AssemblyVersion("12.55.22.0")]
+[assembly: AssemblyFileVersion("12.55.22.0")]
 
 // The following allows the testing assembly to access internal classes such as QueryStringBuilder.
 [assembly: InternalsVisibleTo("MicroFocus.Adm.Octane.Api.Core.Tests")]
