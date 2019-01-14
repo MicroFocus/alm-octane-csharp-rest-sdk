1199056f7054babbbff9fe61ed61cf5813e4d4ad
kdrtibor <kadar-tibor.manasse@microfocus.com>
2019-01-14 17:16:03 +0200

    user story #694431: adds license to files and cleans up code

--- OctaneSdk/Connector/Authentication/SsoAuthenticationStrategy.cs
+++ OctaneSdk/Connector/Authentication/SsoAuthenticationStrategy.cs
@@ -1,12 +1,25 @@
-﻿using MicroFocus.Adm.Octane.Api.Core.Entities;
+﻿/*!
+* (c) 2016-2018 EntIT Software LLC, a Micro Focus company
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using MicroFocus.Adm.Octane.Api.Core.Entities;
 using MicroFocus.Adm.Octane.Api.Core.Services.Core;
 using MicroFocus.Adm.Octane.Api.Core.Services.Version;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
@@ -54,12 +67,6 @@ namespace MicroFocus.Adm.Octane.Api.Core.Connector.Authentication
                     mutex.WaitOne();
                     try
                     {
-                        // do not authenticate if lwssoValue is not empty
-                        if (IsConnected())
-                        {
-                            return true;
-                        }
-
                         ClearSessionFields();
 
                         this.host = host;
@@ -178,7 +185,7 @@ namespace MicroFocus.Adm.Octane.Api.Core.Connector.Authentication
 
         public void OnResponse(HttpWebResponse response)
         {
-            // do nothing yet
+            // don't need to implement this, access tokens do not get refreshed only on connect
         }
         
         public void PrepareRequest(HttpWebRequest request)
