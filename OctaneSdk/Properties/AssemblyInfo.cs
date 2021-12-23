28a16d803f2956e11d1c4b808191555e8f8ca100
Dan Moldoveanu <dan.moldoveanu@microfocus.com>
2021-12-23 11:48:15 +0200

    tech: 16.0.200.1

--- OctaneSdk/Properties/AssemblyInfo.cs
+++ OctaneSdk/Properties/AssemblyInfo.cs
@@ -48,8 +48,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("16.0.200.0")]
-[assembly: AssemblyFileVersion("16.0.200.0")]
+[assembly: AssemblyVersion("16.0.200.1")]
+[assembly: AssemblyFileVersion("16.0.200.1")]
 
 // The following allows the testing assembly to access internal classes such as QueryStringBuilder.
 [assembly: InternalsVisibleTo("MicroFocus.Adm.Octane.Api.Core.Tests")]
