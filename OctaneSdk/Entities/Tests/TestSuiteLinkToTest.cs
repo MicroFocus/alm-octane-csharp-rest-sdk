dc62f19fbe596c33d1af1b9393f10331df462ce7
Mr.Boba <2mr.boba@gmail.com>
2022-04-20 06:09:24 -0700

    new model TestSuiteLinkToTest was added

---
+++ OctaneSdk/Entities/Tests/TestSuiteLinkToTest.cs
@@ -0,0 +1,48 @@
+﻿/*!
+* (c) Copyright 2016-2021 Micro Focus or one of its affiliates.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+
+namespace MicroFocus.Adm.Octane.Api.Core.Entities.Tests
+{
+    public class TestSuiteLinkToTest : BaseEntity
+    {
+        public static string SUBTYPE_FIELD = "subtype";
+
+        public TestSuiteLinkToTest()
+        {
+            AggregateType = "test_suite_link_to_test";
+        }
+
+        public TestSuiteLinkToTest(EntityId id)
+            : base(id)
+        {
+            AggregateType = "test_suite_link_to_test";
+        }
+
+        public string SubType
+        {
+            get
+            {
+                return GetStringValue(SUBTYPE_FIELD);
+            }
+            set
+            {
+                SetValue(SUBTYPE_FIELD, value);
+            }
+
+        }
+    }
+}
